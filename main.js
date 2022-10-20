e06ef1369c1d0eab47eb96b9aa74178d37269c17
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2022-10-20 19:59:12 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// JavaScript код в окремому файлі
+// рядковий коментар
+/*
+    блочний коментар
+*/
+
+// alert - імʼя функції
+// текстове значення огортається в лапки - "blabla" 'blabla'
+
+// відкриття діалогових вікон браузера
+alert("Hello JavaScript!!!");        // показ повідомлення в браузері
+confirm("Do you want to continue?"); // запит користувача на yes/no
+prompt("Enter your name: ");         // отримання значення від користувача
+
+// показ повідомлень в консоль
+console.log("It is a log message in console!"); // інформація
+console.warn("Some warning message!");          // попередження
+console.error("Some error message!");           // помилка
